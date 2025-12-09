ee28df0e33271aac832a84201d31099e0a9ec63a
Gadi Satwika <satwikagadi2005@gmail.com>
2025-12-09 23:56:34 +0530

    Add files via upload

---
+++ Basic_Projects/Super_League_Over_Card/index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className='background-page'>
+    <h1 className='super-heading'>Super Over League</h1>
+    <div class='images'>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+        class='eachTeamImage'
+      />
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+        class='eachTeamImage'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
